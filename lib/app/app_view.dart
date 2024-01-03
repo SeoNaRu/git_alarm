0bbb63bdcfb2fc27cc18a8c65da7dc3dc955476a
SeoNaru <tjehddnr12345@naver.com>
2024-01-03 20:43:29 +0900

    first commit

---
+++ lib/app/app_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:git_commit/login/view/login_view.dart';
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: LoginView());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:git_commit/app/app_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const AppView());
+}
