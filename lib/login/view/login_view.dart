e65cf9d41cb771fd0ddc46aa2a9d61fdaa4c4e28
SeoNaru <tjehddnr12345@naver.com>
2024-01-13 14:16:49 +0900

    fead: login UI

--- lib/login/view/login_view.dart
+++ lib/login/view/login_view.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:git_commit/login/bloc/login_bloc.dart';
-import 'package:git_commit/login/bloc/login_event.dart';
-import 'package:git_commit/login/bloc/login_state.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -14,47 +8,59 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  final TextEditingController _usernameController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('하이'),
-        ),
-        body: BlocBuilder<LoginBloc, LoginState>(
-          builder: (context, state) {
-            return Container(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                      labelText: 'GitHub 사용자 이름',
-                      border: OutlineInputBorder(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 100, 16, 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                '개발자님!\n커밋 습관형성을 위해\n오신 것을 환영해요!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 61,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      // 모서리 둥글게 설정
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'GitHub으로 시작하기',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  if (state.githubLoading)
-                    CircularProgressIndicator()
-                  else
-                    Text(state.commitMessage),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: state.githubLoading
-                        ? null
-                        : () {
-                            context.read<LoginBloc>().add((GitHubName(
-                                nickName: _usernameController.text)));
-                          },
-                    child: Text('Commit 조회하기'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Google 계정으로 시작하기',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
                   ),
                 ],
               ),
-            );
-          },
-        ));
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/login/view/login_view_test.dart
@@ -0,0 +1,60 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:git_commit/login/bloc/login_bloc.dart';
+// import 'package:git_commit/login/bloc/login_event.dart';
+// import 'package:git_commit/login/bloc/login_state.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// class LoginView extends StatefulWidget {
+//   const LoginView({super.key});
+
+//   @override
+//   State<LoginView> createState() => _LoginViewState();
+// }
+
+// class _LoginViewState extends State<LoginView> {
+//   final TextEditingController _usernameController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('하이'),
+//         ),
+//         body: BlocBuilder<LoginBloc, LoginState>(
+//           builder: (context, state) {
+//             return Container(
+//               padding: EdgeInsets.all(16),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   TextField(
+//                     controller: _usernameController,
+//                     decoration: InputDecoration(
+//                       labelText: 'GitHub 사용자 이름',
+//                       border: OutlineInputBorder(),
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   if (state.githubLoading)
+//                     CircularProgressIndicator()
+//                   else
+//                     Text(state.commitMessage),
+//                   const SizedBox(height: 10),
+//                   ElevatedButton(
+//                     onPressed: state.githubLoading
+//                         ? null
+//                         : () {
+//                             context.read<LoginBloc>().add((GitHubName(
+//                                 nickName: _usernameController.text)));
+//                           },
+//                     child: Text('Commit 조회하기'),
+//                   ),
+//                 ],
+//               ),
+//             );
+//           },
+//         ));
+//   }
+// }
