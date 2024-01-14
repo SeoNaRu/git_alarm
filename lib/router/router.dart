96122efd50668e411e5eeafb2595d56c28595d6b
SeoNaru <tjehddnr12345@naver.com>
2024-01-14 22:05:11 +0900

    add:routter controller

---
+++ lib/router/router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RouterControll extends StatefulWidget {
+  const RouterControll({super.key});
+
+  @override
+  State<RouterControll> createState() => _RouterControllState();
+}
+
+class _RouterControllState extends State<RouterControll> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
