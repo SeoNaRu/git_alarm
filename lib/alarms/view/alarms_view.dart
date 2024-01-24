62f5ab9b609009fd92894587e1830be104d64853
SeoNaru <tjehddnr12345@naver.com>
2024-01-24 22:14:55 +0900

    fead : UI

--- lib/alarms/view/alarms_view.dart
+++ lib/alarms/view/alarms_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlarmsView extends StatefulWidget {
@@ -8,13 +9,179 @@ class AlarmsView extends StatefulWidget {
 }
 
 class AalarmViewsState extends State<AlarmsView> {
+  bool isChecked = true;
+  bool issChecked = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Center(child: Text('알람')),
-        ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 36,
+            ),
+            const SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '알림 등록',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromRGBO(157, 157, 157, 1)),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '편집',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        '추가',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              '알림을 등록하여',
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            Text(
+              '꾸준한 커밋 습관을 만들어봐요!',
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(height: 30),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromRGBO(157, 157, 157, 0.5),
+                  width: 0,
+                ),
+              ),
+            ),
+            SizedBox(height: 18),
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '오후 10:30',
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                      Text(
+                        '월, 화, 수, 목, 금',
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                    ],
+                  ),
+                  CupertinoSwitch(
+                    value: isChecked,
+                    activeColor: Color.fromRGBO(157, 157, 157, 1),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value ?? false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 18),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromRGBO(157, 157, 157, 0.5),
+                  width: 0,
+                ),
+              ),
+            ),
+            SizedBox(height: 18),
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '오후 2:30',
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                      Text(
+                        '토, 일',
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(157, 157, 157, 1)),
+                      ),
+                    ],
+                  ),
+                  CupertinoSwitch(
+                    value: issChecked,
+                    activeColor: Color.fromRGBO(157, 157, 157, 1),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        issChecked = value ?? false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromRGBO(157, 157, 157, 0.5),
+                  width: 0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/setting/view/setting_veiw.dart
+++ lib/setting/view/setting_veiw.dart
@@ -10,11 +10,142 @@ class SettingView extends StatefulWidget {
 class _SettingViewState extends State<SettingView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Center(child: Text('세팅')),
-        ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 36,
+            ),
+            SizedBox(
+              child: Text(
+                '설정',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w700,
+                    color: Color.fromRGBO(157, 157, 157, 1)),
+              ),
+            ),
+            SizedBox(
+              height: 96,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              '내 정보',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              '서비스 소개',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              '공지 사항',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              '함께 한 팀원들',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+            SizedBox(
+              height: 18,
+            ),
+            Text(
+              '로그아웃',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color.fromRGBO(157, 157, 157, 1)),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(157, 157, 157, 0.5),
+                    width: 0,
+                  ),
+                )),
+          ],
+        ),
       ),
     );
   }
